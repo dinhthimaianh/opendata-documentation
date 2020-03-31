13f1ade0984f57a37292c877a04dfb8f4ffcac85
Đinh Thị Mai Anh <anhdtm0897@gmail.com>
2020-03-31 17:00:19 +0700

    test

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@ import subprocess
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo',
-    'sphinx.ext.autosummary', ]
+    'sphinx.ext.autosummary', 'toolkit_sphinx_extension',]
 autodoc_member_order = 'bysource'
 todo_include_todos = True
 
